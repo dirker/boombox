c65d432b1b2de5467801963436f19db8e71ba68d
Dirk Hörner <dirker@gmail.com>
2014-10-26 21:27:55 +0100

    Add parsing of rfid/track mapping stored on SD card
    The SD card holds a text file /mappings.txt in the following format, one
    line per entry:
    
    <10 char asciihex rfid tag><space><trackname in 8.3 format>[newline]

--- boombox.ino
+++ boombox.ino
@@ -7,11 +7,91 @@
 #define CARDCS 4
 #define DREQ 3
 
+struct mapping_entry {
+	char tag[10];
+	char track[12];
+};
+
+struct config {
+	struct mapping_entry mapping[5];
+};
+
+static struct config config;
+
 static Adafruit_VS1053_FilePlayer player =
 	Adafruit_VS1053_FilePlayer(SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
+static void mapping_read(struct mapping_entry *mapping, File *file, unsigned max_mappings)
+{
+	struct mapping_entry e;
+	unsigned i, cnt;
+
+	for (cnt = 0; cnt < max_mappings; cnt++) {
+		char *tag = e.tag;
+		char *track = e.track;
+		char c;
+
+		memset(&e, 0, sizeof(e));
+
+		for (i = 0; i < sizeof(e.tag); i++) {
+			c = file->read();
+			if (c == -1)
+				return;
+
+			*tag++ = c;
+		}
+
+		c = file->read();
+		if (c != ' ')
+			return;
+
+		for (i = 0; i < sizeof(e.track); i++) {
+			c = file->read();
+			if (c == -1)
+				break;
+
+			*track++ = c;
+		}
+
+		/* sanity check track */
+		if (e.track[0] == 0)
+			return;
+
+		*mapping++ = e;
+
+		/* swallow until newline */
+		do {
+			c = file->read();
+		} while (c != '\n' && c != -1);
+	}
+}
+
+static void config_dump(struct config *c)
+{
+	struct mapping_entry *map_e = c->mapping;
+	int i;
+
+	Serial.println("[*] mappings:");
+	i = 0;
+	while (*map_e->tag != 0) {
+		char buf[32];
+
+		snprintf(buf, sizeof(buf),
+		         "[%d] %.10s -> %.12s",
+		         i, map_e->tag, map_e->track);
+		Serial.println(buf);
+
+		map_e++;
+		i++;
+	}
+}
+
 void setup() {
+	File f;
+
 	Serial.begin(57600);
+	Serial.println();
+	Serial.println();
 
 	player.begin();
 	SD.begin(CARDCS);
@@ -19,6 +99,14 @@ void setup() {
 	player.setVolume(60, 60);
 	player.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
 	player.playFullFile((char *)"startup.mp3");
+
+	f = SD.open("mapping.txt", FILE_READ);
+	if (f) {
+		mapping_read(config.mapping, &f, 5);
+		f.close();
+	}
+
+	config_dump(&config);
 }
 
 void loop() {
